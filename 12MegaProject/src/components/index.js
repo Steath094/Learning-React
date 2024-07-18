26e7a027ca5b388b090bd47c8df596979ec64a6d
Sam <sameer1321khan@gmail.com>
2024-07-18 16:19:20 +0530

    Created Some Components

---
+++ 12MegaProject/src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container"
+import Logo from "./Logo";
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
--- 12MegaProject/src/conf/conf.js
+++ 12MegaProject/src/conf/conf.js
@@ -5,7 +5,6 @@ const conf = {
     appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID),
 
-
 }
 
 export default conf
