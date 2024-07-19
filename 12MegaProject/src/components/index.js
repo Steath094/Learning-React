a0259c2267af5b7f8a1253ae07578f727c110c9b
Sam <sameer1321khan@gmail.com>
2024-07-19 21:07:10 +0530

    Created Login and Signup using reack hook form

--- 12MegaProject/src/components/index.js
+++ 12MegaProject/src/components/index.js
@@ -3,11 +3,23 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container"
 import Logo from "./Logo";
-
+import Button from "./Button"
+import Input from "./Input"
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Login from "./Login";
+import Signup from "./Signup";
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
+    PostCard,
+    Select,
+    Login,
+    Signup,
+    //... other components go here...
 }
